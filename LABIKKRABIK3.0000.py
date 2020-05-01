593f016c4567a7b6fb03b66bb9007ee6519fc3b1
Techno-420 <54204064+Techno-420@users.noreply.github.com>
2020-05-01 19:29:08 +0300

    Coooooomit

--- LABIKKRABIK3.0000.py
+++ LABIKKRABIK3.0000.py
@@ -1,1 +1,18 @@
 labirint = open('labirint.txt','r')
+lab = []
+for lines in labirint:
+    lab.append([lines])
+for i in range(len(lab)):
+    for el in lab[i][0]:
+        if el != '\n' :
+            lab[i].append(el)
+    del lab[i][0]
+n,m = map(int, input("Введите индексы старта").split())
+k,l = map(int, input("Введите индексы финиша").split())
+lab[n][m],lab[k][l] = 0,1
+for line in lab:
+    print()
+    for el in line:
+        print(el,end=' ')
+
+
