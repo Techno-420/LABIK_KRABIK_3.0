8617f507b037e71466dcb7a566369aa844b8498b
Techno-420 <54204064+Techno-420@users.noreply.github.com>
2020-05-08 09:15:09 +0300

    Last Commit

--- LABIKKRABIK3.0000.py
+++ LABIKKRABIK3.0000.py
@@ -8,13 +8,9 @@ for i in range(len(lab)):
         if el != '\n' :
             lab[i].append(el)
     del lab[i][0]
-n,m = map(int, input("Введите индексы старта").split())
-k,l = map(int, input("Введите индексы финиша").split())
+n,m = map(int, input("Введите индексы старта:").split())
+k,l = map(int, input("Введите индексы финиша:").split())
 lab[n][m],lab[k][l] = 0,1
-for line in lab:
-    print()
-    for el in line:
-        print(el,end=' ')
 
 
 def neighbor(open, closed, arr, indexI, indexJ):
@@ -25,7 +21,7 @@ def neighbor(open, closed, arr, indexI, indexJ):
     neighborindex.append([indexI,indexJ-1])
     for i in range(len(neighborindex)):
         el = arr[neighborindex[i][0]][neighborindex[i][1]]
-        if (el in open) or (el in closed) or (el=='x'):
+        if  ([neighborindex[i][0],neighborindex[i][1]] in closed) or (isinstance(el, str)==True and el != ' '):
             neighborindex[i] = 0
     while 0 in neighborindex:
         for el in neighborindex:
@@ -38,48 +34,64 @@ def h(j2,j1,i2,i1):
     return sqrt((j2-j1)**2+(i2-i1)**2)
 
 
-'''
-def min_f(open):
-    
-    ці дві функції доробить
-    
-def reconstruct_path(cameFrom, current)
-'''
+def minf(fmatr,gmatr,neib,indI,indJ,closed,cur):
+    f_arr = []
+    if len(neib)==1:
+        gmatr[neib[0][0]][neib[0][1]] = gmatr[cur[0]][cur[1]] + 1
+        fmatr[neib[0][0]][neib[0][1]] = gmatr[neib[0][0]][neib[0][1]] + h(indI, neib[0][0], indJ, neib[0][1])
+        indexI = neib[0][0]
+        indexJ = neib[0][1]
+    else:
+        for neibhor in neib:
+            gmatr[neibhor[0]][neibhor[1]] = gmatr[cur[0]][cur[1]] + 1
+            fmatr[neibhor[0]][neibhor[1]] = gmatr[neibhor[0]][neibhor[1]] + h(indI, neibhor[0], indJ, neibhor[1])
+            f_arr.append(fmatr[neibhor[0]][neibhor[1]])
+        el = min(f_arr)
+        for i in range(len(fmatr)):
+            for j in range(len(fmatr[i])):
+                if fmatr[i][j] == el:
+                    indexI = i
+                    indexJ = j
+        for neibhor in neib:
+            if neibhor != [indexI, indexJ]:
+                closed.append(neibhor)
+    return [indexI,indexJ]
 
-def AZIRKA(indIstart,indJstart,indIend,indJend):
+
+def AZIRKA(indIstart,indJstart,indIend,indJend,labirint):
     closed=[]
     open=[[indIstart,indJstart]]
-    matrixfrom = [[0 for i in range(len(lab))] for j in range(len(lab))]
-    g = [['inf' for i in range(len(lab))] for j in range(len(lab))]
-    g[indIstart][indJstart]=0
-    f=[[True for i in range(len(lab))] for j in range(len(lab))]
+    g = [[0 for i in range(len(lab))] for j in range(len(lab))]
+    g[indIstart][indJstart] = 0
+    f = [[0 for i in range(len(lab))] for j in range(len(lab))]
+    f_arr = []
+    count = 0
     f[indIstart][indJstart] = h(indJend, indJstart, indIend, indIstart)
-    for i in matrixfrom:
-        print()
-        for j in i:
-            print('%3s'%j, end=' ')
-    for i in g:
-        print()
-        for j in i:
-            print('%3s'%j, end=' ')
-    for i in f:
-        print()
-        for j in i:
-            print('%0.2f'%j, end=' ')
-    curr=min_f(open)                                     ##min_f(open) треба зробить
+    curr=[indIstart, indJstart]
     goal=[indIend,indJend]
     while open:
-        if curr==goal:
-            return reconstruct_path(cameFrom, current)   ##reconstruct_path(cameFrom, current) треба зробить
+        if curr == goal:
+            labirint[goal[0]][goal[1]] = chr(87+count)
+            for line in labirint:
+                print()
+                for el in line:
+                    print(el, end=' ')
+            break
+        neibh = neighbor(open,closed,labirint,curr[0],curr[1])
+        curr = minf(f,g,neibh,indIend,indJend,closed,curr)
+        open.append(curr)
+        closed.append(open[0])
+        del open[0]
+        if count<9:
+            labirint[curr[0]][curr[1]] = chr(49+count)
+        else:
+            labirint[curr[0]][curr[1]] = chr(88+count)
+        count+=1
 
-        del open[open.index(curr)]
-        for neighbour in neighbor(open, closed, lab, curr[0], curr[1]):
-            temp_g=g[curr[0]][curr[1]]+1
-            if neighbour not in open or temp_g<g[neighbour[0]][neighbour[1]]:
-                matrixfrom[neighbour[0]][neighbour[1]]=curr
-                g[neighbour[0]][neighbour[1]]=temp_g
-                f[neighbour[0]][neighbour[1]]=g[neighbour[0]][neighbour[1]]+h(indJend,neighbour[1],indIend,neighbour[0])
-                if neighbour not in open:
-                    open.append(neighbour)
 
-AZIRKA(6,1,1,6,)
+AZIRKA(n,m,k,l,lab)
+
+
+
+
+
